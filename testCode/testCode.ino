214b03bc4459c2bd781ffd9cdd62bfc002c7e232
fosu562 <Franklino0808@gmail.com>
2023-03-09 15:58:55 +1300

    Added test code to check inputs and try new methods of writing the code

---
+++ testCode/testCode.ino
@@ -0,0 +1,38 @@
+// Code to control individually adressable neopixels throught the use of an infared sensor and a remote. WS2812 lights
+// Running on 5V from an arduino uno board
+// Will need to install adafruit neopixel library and IRRemote library
+// Author: Fork
+
+// Include the infared sensor library
+#include <IRremote.h>
+
+// Recieve pin for the infared sensor
+int RECV_PIN = 11;
+
+// Set up the system
+void setup() {
+  Serial.begin(9600); // Begin the serial monitor
+  IrReceiver.begin(RECV_PIN, ENABLE_LED_FEEDBACK); // Start the IR reciever
+}
+
+void loop() {
+  // Franklins number values
+  // 1: 0xC
+  // 2: 0x18
+  // 3: 0x5E
+  // 4: 0x8
+  // 5: 0x1C
+  // 6: 0x5A
+  // 7: 0x42
+  // 8: 0x52
+  // 9: 0x4A
+  // If there is input to the sensor
+  if(IrReceiver.decode()){
+
+    // Print the code to the output
+    Serial.println(IrReceiver.decodedIRData.command, HEX);
+    IrReceiver.resume(); // Resume searching for values
+    delay(100); // Delay for 100ms
+  } 
+}
+
